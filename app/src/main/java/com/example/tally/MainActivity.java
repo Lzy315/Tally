4ad97462876f7dba230e22987c4a67e6abffc6d8
lzy1107 <liangzhiyi315@163.com>
2022-03-27 22:17:20 +0800

    commit 20220327 22:20

--- app/src/main/java/com/example/tally/MainActivity.java
+++ app/src/main/java/com/example/tally/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.tally;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 /*记账本*/
 public class MainActivity extends AppCompatActivity {
+    Button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
